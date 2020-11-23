ff1ad003b2071fbc3b7d6670d13bd9f557ac08ab
Radu Mocanu <radu.ionut.mocanu@gmail.com>
2020-11-23 13:08:10 +0200

    added some frontend components

---
+++ frontend/src/app/services/author-api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorApiService {
+  
+  authors = null;
+
+  constructor(private http: HttpClient) { }
+
+  getAuthors(): Observable<Object> {
+    return this.http.get('https://localhost:8443/authors');
+  }
+
+  getAuthor(authorId: string): Observable<Object> {
+    return this.http.get(`https://localhost:8443/authors/${authorId}`);
+  }
+
+  deleteAuthor(authorId: string): Observable<Object> {
+    return this.http.delete(`https://localhost:8443${authorId}`);
+  }
+
+  putAuthor(authorId: string, authorBody: object): Observable<Object> {
+    return this.http.put(`https://localhost:8443${authorId}`, authorBody);
+  }
+
+  postAuthor(authorBody: object): Observable<Object> {
+    return this.http.put(`https://localhost:8443/authors`, authorBody);
+  }
+}
--- frontend/src/app/services/book-api.service.ts
+++ frontend/src/app/services/book-api.service.ts
@@ -13,4 +13,20 @@ export class BookApiService {
   getBooks(): Observable<Object> {
     return this.http.get('https://localhost:8443/books');
   }
+
+  getBook(bookId: string): Observable<Object> {
+    return this.http.get(`https://localhost:8443/books/${bookId}`);
+  }
+
+  deleteBook(bookId: string): Observable<Object> {
+    return this.http.delete(`https://localhost:8443${bookId}`);
+  }
+
+  putBook(bookId: string, bookBody: object): Observable<Object> {
+    return this.http.put(`https://localhost:8443${bookId}`, bookBody);
+  }
+
+  postBook(bookBody: object): Observable<Object> {
+    return this.http.put(`https://localhost:8443/books`, bookBody);
+  }
 }
